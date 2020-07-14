2ad19c2a61cbd58210ef23e0b04053bc96f44c16
Imran Kocabiyik <imrankocabiyik@gmail.com>
2020-07-14 13:49:24 +0200

    Simplyf

--- ec2-user-data-sample.sh
+++ ec2-user-data-sample.sh
@@ -9,11 +9,5 @@ yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
 
-#Install git in your EC2 instance
-sudo yum install git -y
-
-git clone https://github.com/kocabiyik/aws-ml
-
-cat aws-ml/bootstrap1.html > /var/www/html/index.html
+echo "<h1>EC2 Instance:<h1>" > /var/www/html/index.html
 echo "$(hostname -f)" >> /var/www/html/index.html
-cat bootstrap2.html >> /var/www/html/index.html
